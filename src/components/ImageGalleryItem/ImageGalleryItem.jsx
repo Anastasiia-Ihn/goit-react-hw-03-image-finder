62a4e0be48ccdba9c05a9b2ac0a0486da7d5ed0f
Anastasiia <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-10-12 22:57:21 +0200

    changed ImageGalleryItem(class)

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,22 +1,47 @@
+import { Component } from "react";
 import { Modal } from "../Modal/Modal.jsx";
 import { ImgGallery, ItemGallery } from "./ImageGalleryItem.js";
 
-export const ImageGalleryItem = ({ dataForCard }) => {
+// export const ImageGalleryItem = ({ dataForCard }) => {
   
-  // state = {
-  //   isModalOpen: false,
-  // };
+//   // state = {
+//   //   isModalOpen: false,
+//   // };
 
-  // openModal = () => {
-  //   this.setState({ isModalOpen: true });
-  // };
+//   // openModal = () => {
+//   //   this.setState({ isModalOpen: true });
+//   // };
 
-  // closeModal = () => {
-  //   this.setState({ isModalOpen: false });
-  // };
-  return (<ItemGallery>
-    <ImgGallery src={dataForCard.webformatURL}></ImgGallery>
-    {/* <Modal data = {dataForCard }></Modal> */}
-  </ItemGallery>)
+//   // closeModal = () => {
+//   //   this.setState({ isModalOpen: false });
+//   // };
+//   return (<ItemGallery>
+//     <ImgGallery src={dataForCard.webformatURL}></ImgGallery>
+//     {/* <Modal data = {dataForCard }></Modal> */}
+//   </ItemGallery>)
+//   };
+  
+
+export class ImageGalleryItem extends Component {
+  state = {
+    isModalOpen: false,
   };
   
+
+  
+  openModal = () => {
+    this.setState({ isModalOpen: true });
+  };
+  
+  closeModal = () => {
+    this.setState({ isModalOpen: false });
+  };
+  
+  render() {
+    const { isModalOpen } = this.state;
+    const {dataForCard } = this.props;
+  
+    return (<ItemGallery>
+      <ImgGallery src={dataForCard.webformatURL}></ImgGallery>
+    {/* <Modal data = {dataForCard }></Modal> */}
+  </ItemGallery>)}}
