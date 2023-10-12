28c7f194cfdbddf06d355fcd5f1589d3d6a9a616
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-10-12 14:20:21 +0200

    added comps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+// import { Modal } from './Modal/Modal';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    galleryItem: [],
+    loader: false,
+    error: false,
+  };
+
+  render() {
+    return (
+      <>
+        <Searchbar></Searchbar>
+        {this.state.galleryItem.length > 0 && <ImageGallery />}
+        {/* <ImageGalleryItem></ImageGalleryItem>
+          <Modal></Modal> */}
+
+        <Loader></Loader>
+        <Button></Button>
+      </>
+    );
+  }
+}
